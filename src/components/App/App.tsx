f0899e85e6ceb9eb4598c0c69e3c03841798d325
Marianna Milovanova <mmilovanova@kse.org.ua>
2020-04-25 11:09:38 +0300

    feat: add login logout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,11 +3,16 @@ import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 import './App.scss';
 import HomePage from '../HomePage/HomePage';
 import Header from '../Header/Header';
-import AboutUs from '../AboutUs/AboutUs'
+import AboutUs from '../AboutUs/AboutUs';
 
 //here will be all routes
+declare global {
+  const gapi: any;
+}
 
 function App() {
+  console.warn(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
   return (
     <div className="App">
       <Header />
