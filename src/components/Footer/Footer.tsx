f85613abb367a983e13fdb33354464fb09674ddb
OlegBul <olegbul1@gmail.com>
2020-04-25 14:04:09 +0300

    - footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,17 @@
 /*contact us*/
 
-export {}
+import React, {PureComponent} from 'react';
+import './Footer.scss';
+
+export default class Footer extends PureComponent {
+	render() {
+		return (
+			<div className="Footer">
+				<p>
+					© 2020 Team CURWID-19
+					<a href="/">Contact Us</a> 
+				</p>
+			</div>
+		);
+	  }
+}
