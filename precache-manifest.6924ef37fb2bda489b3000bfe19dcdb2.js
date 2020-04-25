bff043cff66f8fb1268236dfa2d4c7da956f6be7
Marianna Milovanova <mmilovanova@kse.org.ua>
2020-04-25 20:03:41 +0300

    Updates

---
+++ precache-manifest.6924ef37fb2bda489b3000bfe19dcdb2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade3dd6797de4089a1572fe710682e7c",
+    "url": "/KURWID-19/index.html"
+  },
+  {
+    "revision": "f18bbea8a132f86b52f2",
+    "url": "/KURWID-19/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0f9cef308d6d6b701c89",
+    "url": "/KURWID-19/static/css/main.7ed3549e.chunk.css"
+  },
+  {
+    "revision": "f18bbea8a132f86b52f2",
+    "url": "/KURWID-19/static/js/2.faca6ed6.chunk.js"
+  },
+  {
+    "revision": "151bc3f104eaec60bd17c7d8d6304c56",
+    "url": "/KURWID-19/static/js/2.faca6ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9cef308d6d6b701c89",
+    "url": "/KURWID-19/static/js/main.6af65fc0.chunk.js"
+  },
+  {
+    "revision": "7a06be2cac70f45c546a",
+    "url": "/KURWID-19/static/js/runtime-main.dd6a5a0e.js"
+  },
+  {
+    "revision": "20b1f1d4dcd1546b57f81639769079d8",
+    "url": "/KURWID-19/static/media/comingsoon-square.20b1f1d4.png"
+  },
+  {
+    "revision": "943134e20cc089372a1a088cd9c2a110",
+    "url": "/KURWID-19/static/media/logo.943134e2.png"
+  }
+]);
